267da00fd3c58350b8903832057aef6c8fe4d941
Fatih20 <fnri39@protonmail.com>
2022-03-20 14:43:30 +0700

    Add a unit type.

--- src/utilities/types.ts
+++ src/utilities/types.ts
@@ -1,1 +1,3 @@
 export type inputConnectedVariable = "" | number;
+
+export type unitPrefix = [-12, "p", "pico"] | [-9, "n", "nano"] | [-6, "&mu;", "micro"] | [-3, "m", "mili"] | [0, "", ""] | [3, "k","kilo"] | [6, "M", "mega"] | [9, "G", "giga"];
