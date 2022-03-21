bc0525fe888400ea6ce6630072e5c3e878db8b8a
Fatih20 <fnri39@protonmail.com>
2022-03-21 12:19:10 +0700

    Fix a buig withing unit conversion logic.

--- src/utilities/conversionLogic.ts
+++ src/utilities/conversionLogic.ts
@@ -24,7 +24,7 @@ export function properUnitConverter (givenResistance : number, givenResistanceUn
 
     const smallestPowerDifference = differenceBetweenUnitPowerArray.reduce((previousValue : number, currentValue : number) => {
         if ((Math.abs(currentValue) < Math.abs(previousValue))) {
-            if ((closestPower >= 0 && currentValue + closestPower > 0) || (closestPower < 0 && currentValue + closestPower <= 0)) {
+            if ((closestPower > 0 && currentValue + closestPower > 0) || (closestPower <= 0 && currentValue + closestPower <= 0)) {
                 return currentValue
             }
         } 
