1537be30ff2cb2ba8b8690a68987976e48254756
Fatih20 <fnri39@protonmail.com>
2022-03-22 08:57:10 +0700

    Add functional button that open and closes the panel.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { useState } from "react";
 
-import { GlobalTransition } from "./GlobalComponent";
+import { GlobalTransition, VanillaButton } from "./GlobalComponent";
 import MainConversion from "./components/main";
 import Header from "./components/header";
 import ControlPanel from "./components/controlPanel";
 import DecimalPlaceContextProvider from "./context/decimalPlace";
 import ComponentUsedContextProvider from "./context/componentUsed";
 import InitialStateContextProvider from "./context/initialState";
+
+interface IControlPanelContainer {
+  isOpen: boolean;
+}
 
 const Main = styled.div`
   color: white;
@@ -16,20 +21,48 @@ const Main = styled.div`
   position: relative;
 `;
 
-const ControlPanelContainer = styled.div`
+const ClosedControlPanel = css`
+  background-color: rgba(0, 0, 0, 0);
+  pointer-events: none;
+`;
+
+const OpenControlPanel = css`
   background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1));
-  bottom: 0;
+  pointer-events: auto;
+`;
+
+const ControlPanelContainer = styled.div<IControlPanelContainer>`
+  box-sizing: border-box;
   color: white;
   display: flex;
+  height: 100vh;
   justify-content: center;
   left: 0;
-  position: absolute;
+  padding: 1em;
+  position: fixed;
   right: 0;
-  top: 0;
   z-index: 20;
+
+  ${({ isOpen }) =>
+    isOpen
+      ? OpenControlPanel
+      : ClosedControlPanel}/* border: solid 1px white; */
+`;
+
+const ControlPanelButton = styled(VanillaButton)`
+  align-self: flex-end;
+  border: solid 1px #000000;
+  border-radius: 5px;
+  background-color: white;
+  color: #333333;
+  filter: drop-shadow(0 3px 5px #0000007a);
+  font-size: 1em;
+  padding: 7px;
+  pointer-events: auto;
 `;
 
 function App() {
+  const [controlPanelOpen, setControlPanelOpen] = useState(true);
   return (
     <>
       <GlobalTransition />
@@ -37,8 +70,14 @@ function App() {
         <InitialStateContextProvider>
           <ComponentUsedContextProvider>
             <DecimalPlaceContextProvider>
-              <ControlPanelContainer>
-                <p>Bruh</p>
+              <ControlPanelContainer isOpen={controlPanelOpen}>
+                <ControlPanelButton
+                  onClick={() =>
+                    setControlPanelOpen((prevControlPanel) => !prevControlPanel)
+                  }
+                >
+                  {controlPanelOpen ? "Close" : "Options"}
+                </ControlPanelButton>
               </ControlPanelContainer>
               <Header />
               <ControlPanel />
